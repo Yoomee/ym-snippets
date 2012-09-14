05f372b92991b74336beee2efc09397db0872b20
Edward Andrews <edward@yoomee.com>
2012-09-14 11:11:18 +0100

    Only display edit link if allowed

--- app/helpers/ym_snippets/snippets_helper.rb
+++ app/helpers/ym_snippets/snippets_helper.rb
@@ -3,7 +3,7 @@ module YmSnippets::SnippetsHelper
   def edit_snippet_link(slug, options={})
     options.reverse_merge!(:link_text => "edit")
     snippet = YmSnippets::Snippet.find_or_create_by_slug(slug)
-    link_to(options[:link_text], edit_snippet_path(snippet), options)
+    link_to(options[:link_text], edit_snippet_path(snippet), options) if can?(:edit, snippet)
   end
   
   def snippet_text(slug)
