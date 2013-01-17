725d69c4ede0508531a994bc59ca50f1e49161b3
Matt Atkins <matt@yoomee.com>
2013-01-17 12:01:06 +0000

    Add default snippet text as optional second argument to snippet_text()

--- app/helpers/ym_snippets/snippets_helper.rb
+++ app/helpers/ym_snippets/snippets_helper.rb
@@ -6,8 +6,13 @@ module YmSnippets::SnippetsHelper
     link_to(options[:link_text], edit_snippet_path(snippet), options) if can?(:edit, snippet)
   end
   
-  def snippet_text(slug)
-    YmSnippets::Snippet.find_or_create_by_slug(slug).to_s
+  def snippet_text(slug,text=nil)
+    snippet = YmSnippets::Snippet.find_or_initialize_by_slug(slug)
+    if snippet.new_record?
+      snippet.text = text
+      snippet.save
+    end
+    snippet.to_s
   end
 
 end
