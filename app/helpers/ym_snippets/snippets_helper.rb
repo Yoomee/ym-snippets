92d3bde1531441336f1065701f5a3920d8c3dd4d
Edward Andrews-Hodgson <edward@yoomee.com>
2013-07-19 10:13:31 +0100

    slug.to_s - work with squeel

--- app/helpers/ym_snippets/snippets_helper.rb
+++ app/helpers/ym_snippets/snippets_helper.rb
@@ -2,12 +2,12 @@ module YmSnippets::SnippetsHelper
 
   def edit_snippet_link(slug, options={})
     options.reverse_merge!(:link_text => "edit")
-    snippet = YmSnippets::Snippet.find_or_create_by_slug(slug)
+    snippet = YmSnippets::Snippet.find_or_create_by_slug(slug.to_s)
     link_to(options[:link_text], edit_snippet_path(snippet), options) if can?(:edit, snippet)
   end
   
   def snippet_text(slug,text=nil)
-    snippet = YmSnippets::Snippet.find_or_initialize_by_slug(slug)
+    snippet = YmSnippets::Snippet.find_or_initialize_by_slug(slug.to_s)
     if snippet.new_record?
       snippet.text = text
       snippet.save
